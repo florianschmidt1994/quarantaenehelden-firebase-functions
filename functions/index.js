9f2f12f76eaf8c9b532bf632ad1cfd22e5407fad
keno dressel <aeternity@keno.digital>
2020-03-15 19:37:09 +0100

    adds askForHelpCreate

--- functions/index.js
+++ functions/index.js
@@ -5,8 +5,9 @@ const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(
   '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',
 );
+const { GeoCollectionReference } = require('geofirestore');
 
-exports.firestoreRequest = functions.region('europe-west1').firestore.document('/ask-for-help/{requestId}/offer-help/{offerId}')
+exports.offerHelpCreate = functions.region('europe-west1').firestore.document('/ask-for-help/{requestId}/offer-help/{offerId}')
   .onCreate(async (snap, context) => {
 
     const parentPath = snap.ref.parent.path; // get the id
@@ -20,6 +21,7 @@ exports.firestoreRequest = functions.region('europe-west1').firestore.document('
     const data = await admin.auth().getUser(uid);
     const { email: receiver } = data.toJSON();
     const { answer, email } = offer.data();
+
     console.log({
       to: receiver,
       from: email,
@@ -28,21 +30,73 @@ exports.firestoreRequest = functions.region('europe-west1').firestore.document('
         subject: 'QuarantäneHelden - Jemand hat dir geschrieben!',
         answer,
         email,
-        request
-      }
-    })
+        request,
+      },
+    });
     return await sgMail.send({
       to: receiver,
-      from: "help@quarantaenehelden.org",
-        replyTo : {
-          email: email
-        },
+      from: 'help@quarantaenehelden.org',
+      replyTo: {
+        email: email,
+      },
       templateId: 'd-ed9746e4ff064676b7df121c81037fab',
       dynamic_template_data: {
         subject: 'QuarantäneHelden - Jemand hat dir geschrieben!',
         answer,
         email,
-        request
-      }
+        request,
+      },
     });
+  });
+
+exports.askForHelpCreate = functions.region('europe-west1').firestore.document('/ask-for-help/{requestId}')
+  .onCreate(async (snap, context) => {
+    const MAPS_ENABLED = false;
+    const parentPath = snap.ref.parent.path; // get the id
+    const askForHelpId = snap.id; // get the id
+    const db = admin.firestore();
+
+    const askForHelpSnap = await db.collection(parentPath).doc(askForHelpId).get();
+    const askForHelpSnapData = askForHelpSnap.data();
+
+    // Create a GeoCollection reference
+    let queryResult;
+    if(MAPS_ENABLED) {
+      const offersRef = new GeoCollectionReference(db.collection('offer-help'));
+      const query = offersRef.near({ center: askForHelpSnapData.coordinates, radius: 30 });
+      queryResult = (await query.get()).docs.map(doc => doc.data());
+
+    } else {
+      const offersRef = db.collection('offer-help');
+      queryResult = (await offersRef.get()).docs.map(doc => (doc.data()).d).filter(data => data.plz.indexOf(askForHelpSnapData.d.plz.slice(0,2)) === 0);
+    }
+    let offersToContact = [];
+    if (queryResult.length > 15) {
+      for (let i = queryResult.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * i);
+        const temp = queryResult[i];
+        queryResult[i] = queryResult[j];
+        queryResult[j] = temp;
+      }
+      offersToContact = queryResult.slice(0, 15);
+    } else {
+      offersToContact = queryResult;
+    }
+
+    await Promise.all(offersToContact.map(async offerDoc => {
+      const { uid } = offerDoc;
+      const offeringUser = await admin.auth().getUser(uid);
+      const { email } = offeringUser.toJSON();
+      return await sgMail.send({
+        to: email,
+        from: 'help@quarantaenehelden.org',
+        templateId: 'd-9e0d0ec8eda04c9a98e6cb1edffdac71',
+        dynamic_template_data: {
+          subject: 'QuarantäneHelden - Jemand braucht deine Hilfe!',
+          request: askForHelpSnapData.d.request,
+          location: askForHelpSnapData.d.location,
+          link: 'https://www.quarantaenehelden.org/#/offer-help/' + askForHelpId
+        },
+      });
+    }));
   });
