48aea35d30a18c5619ba70c18edd8af7e9c08554
keno dressel <aeternity@keno.digital>
2020-03-19 11:41:37 +0100

    moves mails to schedule function

--- functions/index.js
+++ functions/index.js
@@ -8,6 +8,11 @@ sgMail.setApiKey(
 );
 const { GeoCollectionReference } = require('geofirestore');
 
+const MAX_RESULTS = 30;
+const MAPS_ENABLED = false;
+const MINIMUM_NOTIFICATION_DELAY = 20;
+
+
 exports.offerHelpCreate = functions.region('europe-west1').firestore.document('/ask-for-help/{requestId}/offer-help/{offerId}')
   .onCreate(async (snap, context) => {
     try {
@@ -18,7 +23,7 @@ exports.offerHelpCreate = functions.region('europe-west1').firestore.document('/
 
       const offer = await db.collection(parentPath).doc(offerId).get();
       const askRecord = await askForHelp.get();
-      if (!askRecord.exists)  {
+      if (!askRecord.exists) {
         console.error('ask-for-help at ', snap.ref.parent.parent.path, 'does not exist');
         return;
       }
@@ -52,7 +57,7 @@ exports.offerHelpCreate = functions.region('europe-west1').firestore.document('/
             email,
             request,
           },
-          hideWarnings: true // removes triple bracket warning
+          hideWarnings: true, // removes triple bracket warning
         });
       } catch (err) {
         console.warn(err);
@@ -73,89 +78,118 @@ exports.offerHelpCreate = functions.region('europe-west1').firestore.document('/
     }
   });
 
+exports.sendNotificationEmails = functions.pubsub.schedule('every 5 minutes').onRun(async (context) => {
+  const dist = (search, doc) => {
+    return Math.abs(Number(search) - Number(doc.plz));
+  };
+
+  const db = admin.firestore();
+
+  const getEligibleHelpOffers = async (askForHelpSnapData) => {
+    let queryResult = [];
+    if (MAPS_ENABLED) {
+      const offersRef = new GeoCollectionReference(db.collection('offer-help'));
+      const query = offersRef.near({ center: askForHelpSnapData.coordinates, radius: 30 });
+      queryResult = (await query.get()).docs.map(doc => doc.data());
+
+    } else {
+      const offersRef = db.collection('offer-help');
+      if (!askForHelpSnapData || !askForHelpSnapData.d || !askForHelpSnapData.d.plz) {
+        console.warn('Failed to find plz for ask-for-help ', askForHelpSnapData);
+      } else {
+        const search = askForHelpSnapData.d.plz;
+        const start = search.slice(0, -3) + '000';
+        const end = search.slice(0, -3) + '999';
+        const results = await offersRef.orderBy('d.plz').startAt(start).endAt(end).get();
+        const allPossibleOffers = results.docs.map(doc => ({ id: doc.id, ...doc.data().d })).filter(({ plz }) => plz.length === search.length);
+        const sortedOffers = allPossibleOffers.map(doc => ({ ...doc, distance: dist(search, doc) })).sort((doc1, doc2) => {
+          return doc1.distance - doc2.distance;
+        });
+        if (sortedOffers.length > MAX_RESULTS) {
+          const lastEntry = sortedOffers[MAX_RESULTS];
+          queryResult = sortedOffers.filter(doc => doc.distance <= lastEntry.distance);
+        } else {
+          queryResult = sortedOffers;
+        }
+      }
+    }
+
+    let offersToContact = [];
+    if (queryResult.length > MAX_RESULTS) {
+      for (let i = queryResult.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * i);
+        const temp = queryResult[i];
+        queryResult[i] = queryResult[j];
+        queryResult[j] = temp;
+      }
+      offersToContact = queryResult.slice(0, MAX_RESULTS);
+    } else {
+      offersToContact = queryResult;
+    }
+    return offersToContact;
+  };
+
+  const sendNotificationEmails = (eligibleHelpOffers, askForHelpSnapData, askForHelpId) => {
+    Promise.all(eligibleHelpOffers.map(async offerDoc => {
+      try {
+        const { uid } = offerDoc;
+        const offeringUser = await admin.auth().getUser(uid);
+        const { email } = offeringUser.toJSON();
+        await sgMail.send({
+          to: email,
+          from: 'help@quarantaenehelden.org',
+          templateId: 'd-9e0d0ec8eda04c9a98e6cb1edffdac71',
+          dynamic_template_data: {
+            subject: 'QuarantäneHelden - Jemand braucht deine Hilfe!',
+            request: askForHelpSnapData.d.request,
+            location: askForHelpSnapData.d.location,
+            link: 'https://www.quarantaenehelden.org/#/offer-help/' + askForHelpId,
+          },
+          hideWarnings: true, // removes triple bracket warning
+        });
+
+        await db.collection(`/ask-for-help`).doc(askForHelpId).update({
+          'd.notificationCounter': admin.firestore.FieldValue.increment(1),
+          'd.notificationReceiver': admin.firestore.FieldValue.arrayUnion(uid)
+        });
+      } catch (err) {
+        console.warn(err);
+        if (err.response && err.response.body && err.response.body.errors) {
+          console.warn(err.response.body.errors);
+        }
+      }
+    }));
+  };
+
+  try {
+    const askForHelpSnaps = await db.collection('ask-for-help')
+      .where('d.timestamp', '<=', Date.now() - MINIMUM_NOTIFICATION_DELAY * 60 * 1000)
+      .where('d.notificationCounter', '==', 0)
+      .get();
+
+    // RUN SYNC
+    for (let i = 0; i < askForHelpSnaps.length; i++) {
+      const askForHelpSnap = askForHelpSnaps[i];
+      const askForHelpSnapData = askForHelpSnap.data();
+      const askForHelpId = askForHelpSnap.id;
+      const eligibleHelpOffers = await getEligibleHelpOffers(askForHelpSnapData);
+      await sendNotificationEmails(eligibleHelpOffers, askForHelpSnapData, askForHelpId);
+    }
+
+  } catch (e) {
+    console.error(e);
+  }
+
+});
+
 exports.askForHelpCreate = functions.region('europe-west1').firestore.document('/ask-for-help/{requestId}')
   .onCreate(async (snap, context) => {
-    const MAX_RESULTS = 30;
-
-    const dist = (search, doc) => {
-      return Math.abs(Number(search) - Number(doc.plz));
-    };
-
     try {
-      const MAPS_ENABLED = false;
-      const parentPath = snap.ref.parent.path; // get the id
-      const askForHelpId = snap.id; // get the id
       const db = admin.firestore();
-
+      const askForHelpId = snap.id; // get the id
+      const parentPath = snap.ref.parent.path; // get the id
       const askForHelpSnap = await db.collection(parentPath).doc(askForHelpId).get();
       const askForHelpSnapData = askForHelpSnap.data();
-
-      // Create a GeoCollection reference
-      let queryResult = [];
-      if (MAPS_ENABLED) {
-        const offersRef = new GeoCollectionReference(db.collection('offer-help'));
-        const query = offersRef.near({ center: askForHelpSnapData.coordinates, radius: 30 });
-        queryResult = (await query.get()).docs.map(doc => doc.data());
-
-      } else {
-        const offersRef = db.collection('offer-help');
-        if (!askForHelpSnapData || !askForHelpSnapData.d || !askForHelpSnapData.d.plz) {
-          console.warn('Failed to find plz for ask-for-help ', askForHelpSnapData);
-        } else {
-          const search = askForHelpSnapData.d.plz;
-          const start = search.slice(0, -3) + '000';
-          const end = search.slice(0, -3) + '999';
-          const results = await offersRef.orderBy('d.plz').startAt(start).endAt(end).get();
-          const allPossibleOffers = results.docs.map(doc => ({ id: doc.id, ...doc.data().d })).filter(({ plz }) => plz.length === search.length);
-          const sortedOffers = allPossibleOffers.map(doc => ({ ...doc, distance: dist(search, doc) })).sort((doc1, doc2) => {
-            return doc1.distance - doc2.distance;
-          });
-          if (sortedOffers.length > MAX_RESULTS) {
-            const lastEntry = sortedOffers[MAX_RESULTS];
-            queryResult = sortedOffers.filter(doc => doc.distance <= lastEntry.distance);
-          } else {
-            queryResult = sortedOffers;
-          }
-        }
-      }
-
-      let offersToContact = [];
-      if (queryResult.length > MAX_RESULTS) {
-        for (let i = queryResult.length - 1; i > 0; i--) {
-          const j = Math.floor(Math.random() * i);
-          const temp = queryResult[i];
-          queryResult[i] = queryResult[j];
-          queryResult[j] = temp;
-        }
-        offersToContact = queryResult.slice(0, MAX_RESULTS);
-      } else {
-        offersToContact = queryResult;
-      }
-
-      await Promise.all(offersToContact.map(async offerDoc => {
-        try {
-          const { uid } = offerDoc;
-          const offeringUser = await admin.auth().getUser(uid);
-          const { email } = offeringUser.toJSON();
-          return await sgMail.send({
-            to: email,
-            from: 'help@quarantaenehelden.org',
-            templateId: 'd-9e0d0ec8eda04c9a98e6cb1edffdac71',
-            dynamic_template_data: {
-              subject: 'QuarantäneHelden - Jemand braucht deine Hilfe!',
-              request: askForHelpSnapData.d.request,
-              location: askForHelpSnapData.d.location,
-              link: 'https://www.quarantaenehelden.org/#/offer-help/' + askForHelpId,
-            },
-            hideWarnings: true // removes triple bracket warning
-          });
-        } catch (err) {
-          console.warn(err);
-          if (err.response && err.response.body && err.response.body.errors) {
-            console.warn(err.response.body.errors);
-          }
-        }
-      }));
 
       await db.collection('/stats').doc('external').update({
         askForHelp: admin.firestore.FieldValue.increment(1),
